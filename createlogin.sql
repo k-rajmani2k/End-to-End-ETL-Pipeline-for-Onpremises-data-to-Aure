6b4ce0017ed696321225c5aeda43c6536b5b546b
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2024-10-26 02:16:57 -0400

    Update createlogin.sql
    Code File used to create login and user for database used

--- createlogin.sql
+++ createlogin.sql
@@ -1,5 +1,5 @@
 /* code file for creating login and user for the database */
 
-CREATE LOGIN mrk with Password = "KRBPROJECT"
+CREATE LOGIN mrk with Password = 'KRBPROJECT';
 
 create user mrk for login mrk
